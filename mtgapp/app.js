25ae732260c1fd4ab3b8b130f10645793fec762d
Riley Martinez <riley.m.martinez@gmail.com>
2021-12-05 04:49:12 -0600

    Add mongoose functionality
    Additionally, added SVG requirement and header/footer requirements

--- mtgapp/app.js
+++ mtgapp/app.js
@@ -1,13 +1,55 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sessionConfig = {
+  secret: process.env.KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    next();
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +63,7 @@ app.use(cookieParser());
 // setup static resources
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'public/javascripts')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/img', express.static(path.join(__dirname, 'public/images')));
 
 // setup routers
--- mtgapp/routes/users.js
+++ mtgapp/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('index');
 });
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
+
+router.post('/register', async (req, res, next) => {
+  try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, err => {
+          if (err) return next(err);
+          res.redirect('#');
+      })
+  } catch (e) {
+      res.redirect('/');
+  }
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+  const redirectUrl = req.session.returnTo || '/';
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
+})
 
 module.exports = router;
