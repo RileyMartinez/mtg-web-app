e8a5a4658fb6fb36aac3f562d5b31fe00e5fbdd8
Riley Martinez <riley.m.martinez@gmail.com>
2021-12-14 02:27:20 -0600

    Fix feedback page

--- mtgapp/public/javascripts/getFeedbackData.js
+++ mtgapp/public/javascripts/getFeedbackData.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 fs.readFile('./public/feedbackData.json', (err, data) => {
     if (err) throw err;
-    let club = JSON.parse(data);
-    console.log(club);
+    let feedbackData = JSON.parse(data);
+    console.log(feedbackData);
 });
